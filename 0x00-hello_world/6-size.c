021e43e65d753cc1121052af2237b36ff3b8095a
joseph9916 <ebunopeajayi@gmail.com>
2023-02-18 21:08:59 +0000

    assemble a c file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-#include <conio.h>
 
-int main()
+/**
+ * main - Print of various types
+ * 
+ * Return: 0 Always Succes
+ */
+int main(void)
 {
 	printf("Size of a char: %2d byte(s) \n", sizeof(char));
 	printf("Size of a int: %2d byte(s) \n", sizeof(int));
@@ -9,5 +13,5 @@ int main()
 	printf("Size of a long long int: %2d byte(s) \n", sizeof(long long int));
 	printf("Size of a float: %2d byte(s) \n", sizeof(float));
 
-	return 0;
+	return (0);
 }
