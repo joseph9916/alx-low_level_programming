19bc24ee497d1de72ddcce9472da7315f451d57b
joseph9916 <ebunopeajayi@gmail.com>
2023-02-18 20:17:21 +0000

    puts with betty style coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - How to use puts function to print an output
+ *
+ * Return: Always 0 (Success)
+ */
 int main()
 {
 
 	puts("Programming is like building a multilingual puzzle");
 
-	return 0;
+	return (0);
 }
