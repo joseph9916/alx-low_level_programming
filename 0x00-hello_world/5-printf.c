d1894937c3f3d1230682062829c883fb170eba7e
joseph9916 <ebunopeajayi@gmail.com>
2023-02-18 20:02:34 +0000

    prinf function in betty style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
+/*How to use prinf to print an output*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-	return 0;
+	return (0);
 }
