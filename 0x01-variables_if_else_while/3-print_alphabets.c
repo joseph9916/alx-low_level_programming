0e210e6b165507ca0698bf05602b1772daf1d228
joseph9916 <ebunopeajayi@gmail.com>
2023-02-20 04:45:20 -0800

    Print uppercase alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Print A-Z in uppercase
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void){
+	char ch;
+	for (ch = 'A';ch <= 'Z'; ch++)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
