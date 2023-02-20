f1e4a406cd6f1be66ede4cdecf0e7d8861f340ad
joseph9916 <ebunopeajayi@gmail.com>
2023-02-20 02:09:58 -0800

    print the last digit of the number andcompare it with 0, 5, 6

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+#include <stdlih>
 #include <time.h>
 #include <stdio.h>
 
---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - if the last digit of n is greater than 5:
+ * the string and is greater than 5
+ *
+ * if the last digit of n is 0: the string and is 0:
+ *
+ * if the last digit of n is less than 6 and not 0:
+ * the string and is less than 6 and not 0;
+ *
+ * Return:  Always 0 (Success)
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n % 10 > 5)
+		printf("is %d and is greater than 5\n", n % 10);
+	if (n % 10 == 0)
+		printf("is %d and is 0\n", n % 10);
+	if (n % 10  < 6 && n % 10 != 0)
+		printf("is %d and is less than 6 and not 0\n", n % 10);
+
+	return (0);
+}
