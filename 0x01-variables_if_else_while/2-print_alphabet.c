2c48e278b58e7ccbccb20ef4c8baff38bf20986c
joseph9916 <ebunopeajayi@gmail.com>
2023-02-20 04:38:11 -0800

    Print a-z lowercase with put char

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print alphabet a-z
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
