503a50fd5dd7ef5d312586edc2c73362385e6c7d
joseph9916 <ebunopeajayi@gmail.com>
2023-02-21 18:45:43 +0000

    print _putchar with a function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,9 +8,16 @@
  * Return: 0 Always (Success)
  */
 
-int main(void)
+void _putchar()
 {
-	_putchar();
-
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print _putchar
+ *
+ * Return: Always 0. (Success)
+ */
+
+int main(void)
+{
+	_putchar();
+	return(0);
+}
