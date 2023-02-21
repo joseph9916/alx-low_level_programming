d97a8fc031f979235492cda2ac9046dfd9a801b2
joseph9916 <ebunopeajayi@gmail.com>
2023-02-21 05:23:16 -0800

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print _putchar
+ *
+ * Return: 0 Always (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Code on Nested loops and functions
