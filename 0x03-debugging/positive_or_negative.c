0e3f318af3980cf87dbfd8d2e27ce15e06e42974
joseph9916 <ebunopeajayi@gmail.com>
2023-02-23 01:00:49 +0000

    tests if 0 is positive

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlih>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - if number is greater than 0 print is positive
+ * if number = 0 print is zero
+ * if number is less than zero print negative
+ * Return:  Always 0 (Success)
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	if (n == 0)
+		printf("%d is zero\n", n);
+	if (n < 0)
+		printf("%d is negative\n", n);
+
+	return (0);
+}
