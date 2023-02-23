90253eee124e1258fc99921863a17f63d23f9306
joseph9916 <ebunopeajayi@gmail.com>
2023-02-23 20:26:21 +0000

    Multiplicatioin of two nunmbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - Find the multiplication of two numbers
+ *
+ * @a: first number
+ * @b: Second number
+ * Return: multiplication result
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b
+
+	return (result);
+}
