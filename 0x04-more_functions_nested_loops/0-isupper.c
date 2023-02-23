6b4035440fd36ce66c0308d94f9fc036df5ed793
joseph9916 <ebunopeajayi@gmail.com>
2023-02-23 02:03:18 -0800

    Check if number is Uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Find if the input letter in an uppercase
+ *
+ * Retun: 1 if uppercase 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
