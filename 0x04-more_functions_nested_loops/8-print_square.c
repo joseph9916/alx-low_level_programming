4eeae8f12aea18a25c90d66a2cb6fd896fcf5ed3
joseph9916 <ebunopeajayi@gmail.com>
2023-02-24 04:30:21 +0000

    Print # ntmies and n lines

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - print the # as a square;
+ * @size: number of times and lines to print #
+ * Return: O Always Success
+ */
+
+void print_square(int size)
+{
+
+	int i, j;
+
+	if (size > 0) 
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
