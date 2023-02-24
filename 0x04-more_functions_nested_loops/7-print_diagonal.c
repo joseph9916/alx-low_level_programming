fd87aa56b7a11111b5bcf4e35357c476ca17f568
joseph9916 <ebunopeajayi@gmail.com>
2023-02-24 04:42:16 +0000

    Print \ as a diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print the \ as a diagonal;
+ * @n: number lines to print \
+ * Return: O Always Success
+ */
+
+void print_diagonal(int n)
+{
+
+	int i, j;
+
+	if (n > 0) 
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
