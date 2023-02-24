dcddcf772361893d990dcba12bd81c79074be686
joseph9916 <ebunopeajayi@gmail.com>
2023-02-24 04:14:08 +0000

    Print _ n times

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - print the _*n;
+ * @n: number of times to print _
+ * Return: O Always Success
+ */
+
+void print_line(int n)
+{
+
+	int i;
+	if (n > 0) 
+	{
+		for (i = 1; i <= n; i++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
