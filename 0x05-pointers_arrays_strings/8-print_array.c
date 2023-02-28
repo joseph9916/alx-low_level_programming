17fdce0ab96cfcbe1fea2515aa00ab25bebda800
joseph9916 <ebunopeajayi@gmail.com>
2023-02-28 04:25:05 -0800

    Print characters in an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - Print the values in an array
+ * @a: the array
+ * @n: number of element in the array
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n - 1; i++)
+		printf("%d, ", *(a + i));
+	printf("%d\n", *(a + i++));
+}
