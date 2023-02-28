434f24d24983481321a880432f0ff26202638a00
joseph9916 <ebunopeajayi@gmail.com>
2023-02-28 04:30:09 +0000

    Print a string normally and in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - Prints both string normally and in reverse to stand output
+ * @s: beginning pointer of string
+ */
+
+void rev_string(char *s)
+{
+	char *tmp_address = s;
+
+	while (*s != '\0')
+		_putchar(*s);
+		s++;
+	while (s != tmp_address)
+	{
+		s--;
+		_putchar(*s);
+	}
+	_putchar('\n');
+}
