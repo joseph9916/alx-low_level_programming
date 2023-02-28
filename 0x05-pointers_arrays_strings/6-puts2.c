8b0ced93faf2d7892f3962855af521d1519df905
joseph9916 <ebunopeajayi@gmail.com>
2023-02-28 05:10:04 +0000

    Print a half a string by skipping one character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _puts2 - Prints a half of a string to stand output
+ * puts2 - Prints a half of a string to stand output
  * by printing a character and not printing another
  * @str: beginning pointer of string
  */
 
-void _puts2(char *str)
+void puts2(char *str)
 {
 	int i = 0;
 
@@ -15,6 +15,7 @@ void _puts2(char *str)
 		if (i % 2 == 0)
 			_putchar(*str);
 		str++;
+		i++;
 	}
 	_putchar('\n');
 }
