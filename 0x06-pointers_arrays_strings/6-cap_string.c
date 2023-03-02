5d956632b3d0ed46f33cbe04e4b2e663fc7007fc
joseph9916 <ebunopeajayi@gmail.com>
2023-03-02 04:55:12 -0800

    Capitalize words after separators

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,7 +15,8 @@ char *cap_string(char *str)
 		if (*(str + i) == ',' || *(str + i) == ' ' || *(str + i) == '{'
 		|| *(str + i) == ';' || *(str + i) == '?' || *(str + i) == ')'
 		|| *(str + i) == '\"' || *(str + i) == '}' || *(str + i) == '.'
-		|| *(str + i) == '\t' || *(str + i) == '\n')
+		|| *(str + i) == '\t' || *(str + i) == '\n' || *(str + i) == '('
+		|| *(str + i) == '!')
 			if (*(str + i + 1) >= 'a' && *(str + i + 1) <= 'z')
 				*(str + i + 1) = *(str + i + 1) - 32;
 		i++;
