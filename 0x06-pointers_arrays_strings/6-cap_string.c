aa21809b4216a849f5678b4672e98b3710ab5635
joseph9916 <ebunopeajayi@gmail.com>
2023-03-03 02:20:02 -0800

    Captilize each word in a string based om seperators

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,13 +12,20 @@ char *cap_string(char *str)
 
 	while (*(str + i) != '\0')
 	{
+		if (i == 0)
+		{
+			if (*(str + i) >= 'a' && *(str + i) <= 'z')
+				*(str + i) = *(str + i) - 32;
+		}
 		if (*(str + i) == ',' || *(str + i) == ' ' || *(str + i) == '{'
 		|| *(str + i) == ';' || *(str + i) == '?' || *(str + i) == ')'
 		|| *(str + i) == '\"' || *(str + i) == '}' || *(str + i) == '.'
 		|| *(str + i) == '\t' || *(str + i) == '\n' || *(str + i) == '('
 		|| *(str + i) == '!')
+		{
 			if (*(str + i + 1) >= 'a' && *(str + i + 1) <= 'z')
 				*(str + i + 1) = *(str + i + 1) - 32;
+		}
 		i++;
 	}
 	return (str);
