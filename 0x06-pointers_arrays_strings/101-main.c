8d30952533398e7e5f9f10b2db21932dcafe4610
joseph9916 <ebunopeajayi@gmail.com>
2023-03-03 01:05:15 +0000

    Print variety of numbers

--- 0x06-pointers_arrays_strings/101-main.c
+++ 0x06-pointers_arrays_strings/101-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * main - check the code
@@ -11,7 +12,7 @@ int main(void)
     _putchar('\n');
     print_number(402);
     _putchar('\n');
-    print_number(1024);
+    print_number(INT_MIN);
     _putchar('\n');
     print_number(0);
     _putchar('\n');
--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,14 +11,16 @@ void print_number(int n)
 {
 	/*AN interger to get the integer division of i*/
 	int j = 10;
+	int neg = 0; /*if -ve sub ´tract 1 from its absolute value*/
 
 	if (n < 0)
 	{
+		neg = 1;
 		_putchar('-');
-		n = n * -1;
+		n = ((n + neg) * -1);
 	}
 	if (n >= 0 && n < 10)
-		_putchar('0' + n);
+		_putchar('0' + n + neg);
 	else
 	{
 		while (n / j >= 10)
@@ -29,6 +31,6 @@ void print_number(int n)
 			n = n % j;
 			j = j / 10;
 		}
-		_putchar('0' + (n % 10));
+		_putchar('0' + (n % 10) + neg);
 	}
 }
