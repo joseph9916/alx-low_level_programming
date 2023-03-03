d099c743c1cd051622439cd7181fa1a9b65e5c63
joseph9916 <ebunopeajayi@gmail.com>
2023-03-03 02:08:49 +0000

    attempts

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,39 +12,39 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, len;
-	
+	int i = 0, j = 0; 
+	int len;
+
 	/*Get size n1 and n2*/
-	for (i = 0; *(n1 + i) != '\0'; i++)
-		*(n1 + i) = *(n1 + i) - '0';
-	for (j = 0; *(n2 + j) != '\0'; j++)
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+	{
 		*(n2 + j) = *(n2 + j) - '0';
+		j++;
+	}
 	if ((i >= size_r) || (j >= size_r))
 		return (0);
-	else
+	if (i < j)
+		i = j;
+	len = i;
+	printf("%d",i);
+	for (; i <= 0; i--)
+		*(r + i) = *(r + i) + '0';
+	if (*(r + i) > '9')
 	{
-		*r = *n1 + *n2;
-		if (i < j)
-			i = j;
-		len = i;
-		i--;
-		for (; i <= 0; i--)
-			*(r + i) = *(r + i) + '0';
-		if (*(r + i) > '9')
-		{
-			*(r + i) = *(r + i) - 10;
-			if (i >= 0)
-				*(r + i - 1) = *(r + i -1) + 1;
+		*(r + i) = *(r + i) - 10;
+		if (i >= 0)
+			*(r + i - 1) = *(r + i - 1) + 1;
+		else
+			if (len < size_r)
+			{
+				for (len--; len <= 0; len--)
+					*(r + i + 1) = *(r + i);
+				*r = '1';
+			}
 			else
-				if (len < size_r)
-				{
-					for (len--; len <= 0; len--)
-						*(r + i +1) = *(r + i);
-					*r = '1';
-				}
-				else
-					return (0);
-		}
+				return (0);
 	}
 		return (r);
 }
---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_buffer - Print bytes of file
+ * @b: String to print bytes of
+ * @n: Size o string
+ */
+
+void print_buffer(char *b, int size)
+{
+	/*code goes here*/
+	return(0);
+}
