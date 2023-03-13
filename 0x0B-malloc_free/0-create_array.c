c8716d44e9e02fada7242a46a93a982d9c21b0c1
joseph9916 <ebunopeajayi@gmail.com>
2023-03-13 06:52:30 -0700

    Update malloc size

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	p = malloc(sizeof(c) * (size + 1));
+	p = malloc(sizeof(c) * (size));
 	if (p == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
