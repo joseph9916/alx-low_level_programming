5b16b87514bc52b689deff88e48bfd4899ffcaa8
joseph9916 <ebunopeajayi@gmail.com>
2023-03-14 05:32:20 -0700

    Concantenate all arguement to a string

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - Concantenates arguement strings together
+ * @ac: number of arguements
+ * @av: array of strings
+ * Return: pointer to string
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i, j, k = 0, len = 0;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; *(av[i] + j) != '\0'; j++)
+			;
+		len += j + 1;
+	}
+	str = malloc(sizeof(char) * len);
+	if (str == NULL)
+	{
+		free(str);
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; *(av[i] + j) != '\0'; j++)
+		{
+			str[k] = *(av[i] + j);
+			k++;
+		}
+		str[k] = '\n';
+		k++;
+	}
+	return (str);
+}
