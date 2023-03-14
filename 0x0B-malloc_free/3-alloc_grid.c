daadbf0abfb7ca4eb2b903ec599c3ec6ebbcc0d9
joseph9916 <ebunopeajayi@gmail.com>
2023-03-14 00:48:02 +0000

    Use malloc for 2d array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Allocate a grid with malloc
+ * @width: Width of the grid
+ * @height: height of grid
+ * Return: point to the initial grid
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **grid;
+
+	if (width == 0 || height == 0)
+		return (NULL);
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		*(grid + i) = malloc(sizeof(int) * width);
+		if (*grid == NULL)
+			return (NULL);
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
+	return (grid);
+}
