f0a6fc1bf9a452d3f1250bd51fc807d6278956d6
joseph9916 <ebunopeajayi@gmail.com>
2023-03-15 14:59:29 -0700

    Divide a string to a 2d array

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -3,34 +3,49 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
-char **divide_to_array(char **str_arr, char *str, int len)
+ * divide_to_array - takes the 2d array and puts in the words
+ * ending with a null pointer
+ * @str_arr: 2d array of words
+ * @str: string to divide
+ * @n: no of words
+ * Return: 2d array
+ */
+char **divide_to_array(char **str_arr, char *str, int n)
 {
-	int i = 0,k = 0, j, l;
-	while (i < len)
+	int i = 0, j = 0, k = 0, l;
+
+	while (str[i] != '\0')
 	{
-		while (str[i] == 32 || str[i] == '\t' || str[i] == '\n')
+		if (str[i] != 32 && (str[i - 1] == 32 || i == 0))
 		{
-			i++;
-			printf("%d",i);
+			j = i;
+			while (str[j] != 32 && str[j] != '\0')
+				j++;
+			if (k < n)
+				str_arr[k] = ((char *)malloc(sizeof(char) * (j - i + 1)));
+			if (str_arr[k] == NULL)
+			{
+				free(str_arr[k]);
+				return (NULL);
+			}
+			l = 0;
+
+			while (j > i)
+			{
+				str_arr[k][l] = str[i];
+				l++;
+				i++;
+			}
+			str_arr[k][l] = '\0';
+			k++;
 		}
-		j = i;
-		while (!(str[i] == 32 || str[i] == '\t' || str[i] == '\n'))
+		else
 			i++;
-		str_arr[k] = malloc(sizeof(char) * (i - j + 1));
-		l = 0;
-		while (!(str[j] == 32 || str[j] == '\t' || str[j] == '\n'))
-		{
-			printf("%c",(str[j]));
-			str_arr[k][l] = str[j];
-			l++;
-			j++;
-		}
-		k++;
-		str_arr[k][l] = '\0';
 	}
+	str_arr[k] = NULL;
 	return (str_arr);
 }
-*/
+
 /**
  * strtow - Divide a string of words to an array of words
  * divided by a space
@@ -42,7 +57,7 @@ char **strtow(char *str)
 {
 	/*i,j counting variables,k to increment by words,*/
 	/*l for letters in word, n no of words*/
-	int i = 0, j, l = 0, k = 0, len, n = 0;
+	int i = 0, n = 0;
 	char **str_arr;
 
 	if (str == NULL || str[0] == '\0')
@@ -50,48 +65,16 @@ char **strtow(char *str)
 	/*Find the length of the word*/
 	while (str[i] != '\0')
 	{
-		if (!(str[i] == 32 || str[i] == '\t' || str[i] == '\n'))
-		{
+		if (str[i] != 32 && (str[i - 1] == 32 || i == 0))
 			n++;
-			while (!(str[i] == 32 || str[i] == '\t' || str[i] == '\n'))
-				i++;
-		}
-		else
-			i++;
+		i++;
 	}
-	len = i;
-	printf("%d, %d",len,n);
-	str_arr = malloc(sizeof(char *) * n);
+	str_arr = malloc(sizeof(char *) * (n + 1));
 	if (str_arr == NULL)
 	{
 		free(str_arr);
 		return (NULL);
 	}
-	printf("%s",str);
-	/*str_arr = divide_to_array(str_arr, str, len);*/
-	i = 0;
-	while (i < len)
-        {
-                while (str[i] == 32 || str[i] == '\t' || str[i] == '\n')
-                {
-                        i++;
-                        printf("%d",i);
-                }
-                j = i;
-                while (!(str[i] == 32 || str[i] == '\t' || str[i] == '\n'))
-                        i++;
-                str_arr[k] = malloc(sizeof(char) * (i - j + 1));
-                l = 0;
-                while (!(str[j] == 32 || str[j] == '\t' || str[j] == '\n'))
-                {
-                        printf("%c",(str[j]));
-                        str_arr[k][l] = str[j];
-                        l++;
-                        j++;
-                }
-                k++;
-                str_arr[k][l] = '\0';
-        }
-        return (str_arr);
+	str_arr = divide_to_array(str_arr, str, n);
 	return (str_arr);
 }
