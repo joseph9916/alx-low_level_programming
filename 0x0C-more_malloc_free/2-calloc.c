77d2e23ef833c281f8db4bf3590d470671ba8a2e
joseph9916 <ebunopeajayi@gmail.com>
2023-03-16 21:48:28 +0000

    Calloc implementation using malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * _calloc - Allocates a array of memory of nemb elements of size bytes
+ * @nmemb: number of elements
+ * @size: size of elemeents
+ * Return: pointer to address or NULL if it fails
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i = 0;
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+
+	if (ptr == NULL)
+		return (NULL);
+	while (i < nmemb)
+	{
+		ptr[i] = 0;
+		i++;
+	}
+	return (ptr);
+}
