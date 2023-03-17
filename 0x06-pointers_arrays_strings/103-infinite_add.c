128aac5192c3fa2373bdd970f09607935cf16518
joseph9916 <ebunopeajayi@gmail.com>
2023-03-17 20:33:28 +0000

    Add very large scale numbers with arrays

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,39 +12,43 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0, j = 0; 
-	int len;
+	int len1 = 0, len2 = 0, len_r, i;
 
 	/*Get size n1 and n2*/
-	while (*(n1 + i) != '\0')
-		i++;
-	while (*(n2 + j) != '\0')
-	{
-		*(n2 + j) = *(n2 + j) - '0';
-		j++;
-	}
-	if ((i >= size_r) || (j >= size_r))
+	while (n1[len1] != '\0')
+		len1++;
+	while (n2[len2] != '\0')
+		len2++;
+
+	if ((len1 >= size_r - 1) || (len2 >= size_r - 1))
 		return (0);
-	if (i < j)
-		i = j;
-	len = i;
-	printf("%d",i);
-	for (; i <= 0; i--)
-		*(r + i) = *(r + i) + '0';
-	if (*(r + i) > '9')
+
+
+	for (i = 0; i < size_r; i++)
+		r[i] = '0';
+
+	len_r = size_r;
+	while (len1 > 0 || len2 > 0)
 	{
-		*(r + i) = *(r + i) - 10;
-		if (i >= 0)
-			*(r + i - 1) = *(r + i - 1) + 1;
-		else
-			if (len < size_r)
-			{
-				for (len--; len <= 0; len--)
-					*(r + i + 1) = *(r + i);
-				*r = '1';
-			}
-			else
-				return (0);
+		if (len1 > 0 && len2 > 0)
+			r[size_r - 1] += n1[len1 - 1] + n2[len2 - 1] - 2 * '0';
+		if (len2 <= 0)
+			r[size_r - 1] += n1[len1 - 1]  - '0';
+		if (len1 <= 0)
+			r[size_r - 1] += n2[len2 - 1]  - '0';
+		if (r[size_r - 1] > '9')
+		{
+			r[size_r - 2] += 1;
+			r[size_r - 1] -= 10;
+		}
+		len1--;
+		len2--;
+		size_r--;
 	}
-		return (r);
+	for (i = 0; i < len_r - size_r; i++)
+	{
+		r[i] = r[i + size_r - 1];
+	}
+	r[i + size_r] = '\0';
+	return (r);
 }
