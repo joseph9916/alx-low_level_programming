06e68cdf7d9156593fe50d496c4668563b241193
joseph9916 <ebunopeajayi@gmail.com>
2023-03-23 14:57:58 -0700

    How to use variadic functions

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <strings.h>
+
+/**
+ * print_numbers - Print only numbers
+ * @separator: What separates the numbers being printed like a " "
+ * @n: number of arguements
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i, j = 0;
+
+	va_start(ap, n);
+	if (n == 0)
+		return;
+	for (i = 0; i < n; i++)
+	{
+		if (j != 0 && separator != NULL)
+			printf("%s", separator);
+		printf("%d", va_arg(ap, int));
+			j = 1;
+	}
+	printf("\n");
+	va_end(ap);
+}
