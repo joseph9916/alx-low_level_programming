9f9794f1a530835af3f950a87a0f4369ea5fe1e3
joseph9916 <ebunopeajayi@gmail.com>
2023-03-24 21:24:18 -0700

    Print all from a variadic function

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,56 +4,48 @@
 #include <string.h>
 
 /**
- * prints_all - Prints all strings based on the format sent
+ * print_all - Prints all strings based on the format sent
  * @format: format to be used
  */
 
 void print_all(const char * const format, ...)
 {
 	va_list ap;
-	int i;
-	long unsigned int j = 0;
-	char ch;
-	float f;
+	unsigned long int j = 0;
 	char *str;
 
 	va_start(ap, format);
 	while (format[j] != '\0')
 	{
-		if (format[j] == 'c')
+		switch (format[j])
 		{
-			ch = va_arg(ap, int);
-			printf("%c", ch);
+			case 'c':
+				printf("%c", va_arg(ap, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(ap, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(ap, double));
+				break;
+			case 's':
+				str = va_arg(ap, char *);
+				if (str == NULL)
+					printf("(nil)");
+				if (str != NULL)
+					printf("%s", str);
+				break;
+			default:
+				break;
 		}
-		else if (format[j] == 'i')
-		{
-			i = va_arg(ap, int);
-			printf("%d", i);
-		}
-		else if (format[j] == 'f')
-		{
-			f = va_arg(ap, double);
-			printf("%f", f);
-		}
-		else if (format[j] == 's')
-		{
-			str = va_arg(ap, char *);
-			if (str != NULL)
-				printf("%s", str);
-			else
-				printf("(nil)");
-		}
-		else
-		{
-			;
-		}
-		while (j < (strlen(format) - 1))
+		while (j < (strlen(format) - 1) && (format[j] == 'c' || format[j] == 'i'
+			|| format[j] == 'f' || format[j] == 's'))
 		{
 			printf(", ");
 			break;
 		}
 		j++;
-		
 	}
 	printf("\n");
+	va_end(ap);
 }
