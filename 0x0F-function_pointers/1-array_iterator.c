454608970837f561eeecf87c6b56edb3639005fa
joseph9916 <ebunopeajayi@gmail.com>
2023-03-24 21:31:30 -0700

    NULL condition added

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,6 @@
 #include "function_pointers.h"
 #include <stddef.h>
+#include <stdlib.h>
 
 /**
  * array_iterator - a function that executes a function given as a
@@ -13,6 +14,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned long int i;
 
+	if (array == NULL)
+	{
+		free(array);
+		return;
+	}
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
