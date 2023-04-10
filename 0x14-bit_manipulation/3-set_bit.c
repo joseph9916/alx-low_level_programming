cfb0c8632113e16043eeb124d2e2be790925814e
joseph9916 <ebunopeajayi@gmail.com>
2023-04-10 10:00:26 +0000

    bitwise Manipulations

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * set_bit - set bit at index to 1
+ * @n: number to find bit of
+ * @index: index to get
+ * Return: 1 if it worked else -1
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int i = (1 << index);
+
+	if (n == NULL)
+	{
+		free(n);
+		return (-1);
+	}
+	*n = *n | i;
+	return (1);
+}
