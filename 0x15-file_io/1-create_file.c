0ef08415d945f6d50a26f467c9d613e31a56dadf
joseph9916 <ebunopeajayi@gmail.com>
2023-04-11 14:46:21 +0000

    Create a file with read and write permissions

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * create_file - Create a file where filename is name of the file
+ * @filename: Name of file
+ * @text_content: Content of file
+ * Return: 1 if successful or -1 if it fails
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fp;
+	int bytes_written;
+
+	if (filename == NULL)
+		return (-1);
+	fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	bytes_written = write(fp, text_content, sizeof(text_content));
+	if (bytes_written != -1)
+		return (1);
+	return (-1);
+}
