8f2e8dc793fe9a5c65f74b6bf4cc8b0080ee96f3
joseph9916 <ebunopeajayi@gmail.com>
2023-04-11 09:05:15 +0000

    Read a from a file and prints to stdout

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#define _POSIX_C_SOURCE 200809L
+
+/**
+ * read_textfile - reads a text file and prints 
+ * it to the POSIX standard output
+ * @filename: Name if file to print
+ * @letters: number of letters it should read and print
+ * Return: actual number of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fp;
+	ssize_t n;
+	char buffer[1024];
+
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	fp = open(filename, O_RDONLY);
+	n = read(fp, buffer, letters);
+	write(1, buffer, n);
+
+	close(fp);
+	return (n);
+}
