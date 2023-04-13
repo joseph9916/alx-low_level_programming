9468874c4a002a53bd1dcb58ff5f651020f4a459
joseph9916 <ebunopeajayi@gmail.com>
2023-04-13 11:34:48 +0000

    Create a file in c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 
 	fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (text_content != NULL)
-		bytes_written = write(fp, text_content, (strlen(text_content) + 1));
+		bytes_written = write(fp, text_content, strlen(text_content));
 	close(fp);
 	if (bytes_written != -1)
 		return (1);
