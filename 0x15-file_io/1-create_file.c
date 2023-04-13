f37af30e531f41eea0257f731f99dd6332192f5c
joseph9916 <ebunopeajayi@gmail.com>
2023-04-13 09:15:26 +0000

    Create a file and if it exists truncate it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
 
 /**
  * create_file - Create a file where filename is name of the file
@@ -18,7 +19,8 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	fp = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-	bytes_written = write(fp, text_content, sizeof(text_content));
+	bytes_written = write(fp, text_content, strlen(text_content));
+	printf("%lu, %s\n", strlen(text_content), text_content);
 	if (bytes_written != -1)
 		return (1);
 	return (-1);
