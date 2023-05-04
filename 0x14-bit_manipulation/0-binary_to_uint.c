d5bdc944989eb2f3386618708c36569aa7a01efb
joseph9916 <ebunopeajayi@gmail.com>
2023-05-04 13:23:26 +0000

    Convert binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,32 +1,48 @@
 #include "main.h"
 #include <stddef.h>
-#include <string.h>
+#include <stdio.h>
 
 /**
- * binary_to_uint - Convert binary characters to Unsigned int numbers
- * @b: string to print in binary
- * Return: unsigned int number
+ * power - finds the exponential of n power j
+ * @n: exponent
+ * @j: power
+ * Return: result
+ */
+
+unsigned int power(unsigned int n, unsigned int j)
+{
+	if (j == 0)
+		return (1);
+	else
+		return (n * power(n, j - 1));
+}
+
+/**
+ * binary_to_uint - convert binary to unsigned int of a binary string
+ * @b: binary string b
+ * Return: unsigned int result
  */
 
 unsigned int binary_to_uint(const char *b)
 {
-	int k, i, pw = 1, len;
-	unsigned int result = 0;
-
-	len = strlen(b);
-	k = len - 1;
+	unsigned int result = 0, len = 0, i = 0;
 
 	if (b == NULL)
 		return (0);
-	while (k >= 0)
+	while (*b != '\0')
 	{
-		if (b[k] < 48 || b[k] > 57)
+		if (*b < '0' || *b > '9')
 			return (0);
-		for (i = 1; i < len - k; i++)
-			pw *= 2;
-		result += ((b[k] - 48) * pw);
-		k--;
-		pw = 1;
+		len++;
+		b++;
+	}
+	b--;
+	while (len)
+	{
+		result += ((*b - '0') * power(2, i));
+		b--;
+		len--;
+		i++;
 	}
 	return (result);
 }
