1a7bd58a3a1825811a631e3d0da86b2f70949754
joseph9916 <ebunopeajayi@gmail.com>
2023-08-18 09:40:33 +0000

    Delete at index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -25,9 +25,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (-1);
 	if (curnode == *head)
 	{
-		prevnode = *head;
 		*head = curnode->next;
-		curnode->prev = NULL;
+		*head->prev = NULL;
 		free(prevnode);
 	}
 	else
